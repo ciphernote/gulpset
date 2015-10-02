d817bcd1f6c802a3c07154c090c17b6bd739f119
cipher <ciphernots@gmail.com>
2015-10-02 22:57:16 +0900

    gulpfile.js update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var jade = require('gulp-jade');
 
 /*  Config for your environment */
 
@@ -9,5 +10,13 @@ gulp.task("sass", function() {
         gulp.src("assets/**/*scss")
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(gulp.dest("static/css"));
+        .pipe(gulp.dest("build/static/css"));
+});
+
+gulp.task('jade', function () {
+	gulp.src('assets/**/*.jade')
+	.pipe(jade({
+		pretty: true
+	}))
+	.pipe(gulp.dest('bulid/static/'));
 });
