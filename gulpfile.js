226b40cecbd7d7c1ecf7df07273ceb68f7964d00
ciphernote <ciphernots@gmail.com>
2016-01-23 14:54:24 +0900

    Bump.

--- gulpfile.js
+++ gulpfile.js
@@ -19,38 +19,38 @@ gulp.task("server", function() {
 });
 
 gulp.task('jade', function () {
-	gulp.src('assets/template/*.jade')
-  	.pipe(jade({pretty: true}))
-	.pipe(gulp.dest('./build/static/'))
-	.pipe(browser.reload({stream:true}))
+  gulp.src('assets/template/*.jade')
+  .pipe(jade({pretty: true}))
+  .pipe(gulp.dest('./build/static/'))
+  .pipe(browser.reload({stream:true}))
 });
 
 gulp.task('sass', function() {
-	gulp.src('assets/sass/*scss')
-	.pipe(plumber())
-	.pipe(sass())
-	.pipe(autoprefixer())
-	.pipe(gulp.dest('./build/static/css'))
-	.pipe(browser.reload({stream:true}))
+  gulp.src('assets/sass/*scss')
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./build/static/css'))
+  .pipe(browser.reload({stream:true}))
 });
 
 gulp.task('js', function() {
-	gulp.src('assets/js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('./build/static/js'))
-	.pipe(browser.reload({stream:true}))
+  gulp.src('assets/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./build/static/js'))
+  .pipe(browser.reload({stream:true}))
 });
 
 gulp.task('image', function() {
-	gulp.src('assets/img/*.{png,jpg,jpeg}')
-	.pipe(imagemin())
-	.pipe(gulp.dest('./build/static/img'))
-	.pipe(browser.reload({stream:true}))
+  gulp.src('assets/img/*.{png,jpg,jpeg}')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./build/static/img'))
+  .pipe(browser.reload({stream:true}))
 });
 
 gulp.task("default",['server','jade','sass','js','image'], function() {
-	gulp.watch("assets/js/**/*.js",["js"]);
-	gulp.watch("assets/sass/*.scss",["sass"]);
-	gulp.watch(["assets/template/*.jade","assets/template/**/_*.jade"],["jade"]);
-	gulp.watch("assets/img/*.{png,jpg,jpeg}",["image"]);
+  gulp.watch("assets/js/**/*.js",["js"]);
+  gulp.watch("assets/sass/*.scss",["sass"]);
+  gulp.watch(["assets/template/*.jade","assets/template/**/_*.jade"],["jade"]);
+  gulp.watch("assets/img/*.{png,jpg,jpeg}",["image"]);
 });
